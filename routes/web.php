107d85486b88ad3de8a4385bee202c74a9e7fa27
Ricardo Fernandes <ricardo.fernandes@msn.com>
2019-07-10 12:18:46 -0300

    Limpa as rotas e adiciona rota de produtos

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::resource('produtos', 'ProdutoController');
